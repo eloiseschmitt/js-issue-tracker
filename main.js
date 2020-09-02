29c59fc3d9d658d6be3254322a7122addf66fcef
Eloise Schmitt <eschmitt@atixnet.fr>
2020-09-02 17:00:15 +0200

    factorisation + ajout fonctionnalités et es6

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+'use strict';
+
 function fetchIssues() {
     let issues = JSON.parse(localStorage.getItem('issues'));
     let issuesList = document.getElementById('issuesList');
 
     issuesList.innerHTML = '';
 
-    for(let i = 0 ; i < issues.length ; i++) {
+    for (let i = 0; i < issues.length; i++) {
         let id = issues[i].id;
         let desc = issues[i].description;
         let severity = issues[i].severity;
         let assignedTo = issues[i].assignedTo;
         let status = issues[i].status;
+
+        let textFirstButton;
+        status === 'Closed' ? textFirstButton = 'Open' : textFirstButton = 'Close';
 
         issuesList.innerHTML += `
         <div class="well">
@@ -20,8 +25,8 @@ function fetchIssues() {
                 <span class="glyphicon glyphicon-time></span> ${severity} 
                 <span class="glyphicon glyhicon-user></span> ${assignedTo}
             </p>
-            <a href="#" class="btn btn-warning" onclick="setStatusClosed('${id}')">Close</a>
-            <a href="#" class="btn btn-danger" onclick="deleteIssue('${id}')">Delete</a>
+            <a href="#" class="btn btn-warning" onclick="modifyIssue('close', '${id}')">${textFirstButton}</a>
+            <a href="#" class="btn btn-danger" onclick="modifyIssue('delete', '${id}')">Delete</a>
         </div>
          `
     }
@@ -36,54 +41,37 @@ function saveIssue(e) {
     let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
     let issueStatus = 'Open';
     let issue = {
-      id: issueId,
-      description: issueDesc,
-      severity: issueSeverity,
-      assignedTo: issueAssignedTo,
-      status: issueStatus
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
     }
-    
-    if (localStorage.getItem('issues') === null) {
-        let issues = [];
-      issues.push(issue);
-      localStorage.setItem('issues', JSON.stringify(issues));
-    } else {
-        let issues = JSON.parse(localStorage.getItem('issues'));
-      issues.push(issue);
-      localStorage.setItem('issues', JSON.stringify(issues));
-    }
-    
+
+    let issues;
+    localStorage.getItem('issues') === null ? issues = [] : issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+
     document.getElementById('issueInputForm').reset();
-   
-    fetchIssues();
-    
-    e.preventDefault(); 
-  }
 
-  function setStatusClosed (id) {
-    let issues = JSON.parse(localStorage.getItem('issues'));
-    
-    for(let i = 0; i < issues.length; i++) {
-      if (issues[i].id == id) {
-        issues[i].status = "Closed";
-      }
-    }
-      
-    localStorage.setItem('issues', JSON.stringify(issues));
-    
     fetchIssues();
-  }
 
-  function deleteIssue (id) {
+    e.preventDefault();
+}
+
+
+function modifyIssue(option, id) {
     let issues = JSON.parse(localStorage.getItem('issues'));
-    
-    for(let i = 0; i < issues.length; i++) {
-      if (issues[i].id == id) {
-        issues.splice(i, 1);
-      }
+    for (let i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            if(option == 'close') {
+                issues[i].status == "Closed" ? issues[i].status = "Open" : issues[i].status = "Closed";
+            } else if(option == 'delete') {
+                issues.splice(i, 1);
+            }
+        }
     }
-    
     localStorage.setItem('issues', JSON.stringify(issues));
-    
     fetchIssues();
-  }
+}
